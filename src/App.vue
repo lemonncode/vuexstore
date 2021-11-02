bf6d2942bc9a26f171daa4c2f888f79d81465744
Noelia <lemmonn.87@gmail.com>
2021-11-02 16:25:41 +0100

    vuexstore

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <h1>Todos</h1>
+    <h3>Completed: {{ completedTodos}}</h3>
+    <h3>Pending: {{ pendingTodos }}</h3>
+    <todos-list />
+    <todo-form />
+  </div>
+</template>
+
+<script>
+import TodosList from "./components/TodosList.vue";
+import TodoForm from "./components/TodoForm.vue";
+
+export default {
+  name: "app",
+ 
+  components: {
+    TodosList,
+    TodoForm,
+  },
+  computed: {
+    completedTodos(){
+      return this.$store.getters.completedTodos;
+      
+    },
+     pendingTodos(){
+      return this.$store.getters.pendingTodos;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/TodosList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div><todo v-for="(todo, index) in todos" :key="index" :todo="todo" /></div>
+</template>
+
+<script>
+import Todo from "./Todo.vue";
+
+export default {
+  components: {
+    Todo,
+  },
+  computed: {
+    todos() {
+      return this.$store.state.todos;
+    },
+  },
+};
+</script>
