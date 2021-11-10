812231f84f827a13877c984a59a046f57c98ce38
Noelia <lemmonn.87@gmail.com>
2021-11-10 17:06:43 +0100

    first commit

---
+++ Documents/git_lemonn/vuetify-todo/src/components/Todo/ListTask.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-list class="pt-0" flat>
+    <task v-for="task in $store.state.tasks" :key="task.id" :task="task" />
+  </v-list>
+</template>
+
+<script>
+import Task from "../Todo/Task.vue";
+export default {
+  components: {
+    task: Task,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ Documents/git_lemonn/vuetify-todo/src/views/Todo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <field-add />
+    <list-task v-if="$store.state.tasks.length" />
+    <no-task v-else />
+  </div>
+</template>
+
+<script>
+import FieldAdd from "../components/Todo/FieldAdd.vue";
+import ListTask from "../components/Todo/ListTask.vue";
+import NoTask from "../components/Todo/NoTask.vue";
+
+export default {
+  components: {
+    "field-add": FieldAdd,
+    "list-task": ListTask,
+    "no-task": NoTask,
+  },
+};
+</script>
+
+
